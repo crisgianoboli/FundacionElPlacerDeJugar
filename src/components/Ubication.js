b5d5b5ecbd1407bd2594685f8b9243f2df864f17
Cristian <crisgianoboli@gmail.com>
2021-07-29 17:06:23 -0300

    seccion Home finalizada

--- src/components/Ubication.js
+++ src/components/Ubication.js
@@ -1,31 +1,28 @@
-import React from 'react';
-import './Ubication.css';
-import LogoLP from '../img/Logo Las palmas.png';
-import Map from './Map';
-import Credentials from './Credentials';
+import React from "react";
+import "./Ubication.css";
+import LogoLP from "../img/Logo Las palmas.png";
+import Map from "./Map";
+import Credentials from "./Credentials";
 
 const Ubication = () => {
+  const mapUrl = `https://maps.googleapis.com/maps/api/js?v=3.exp&key=${Credentials.mapsKey}`;
 
-    const mapUrl= `https://maps.googleapis.com/maps/api/js?v=3.exp&key=${Credentials.mapsKey}`;
-
-
-    return (
-      <div className="Ubication-content">
-
-        <div className="logo-content">
-          <img className="logo-lp" src={LogoLP} alt="Logo las palmas" />
-        </div>
-
-        <Map 
-        googleMapURL={mapUrl} 
-        containerElement={ <div style={{ height: '300px' , width: '700px'}} /> }
-        mapElement={ <div style={{ height: '100%' }} /> }
-        loadingElement= { <p>Cargando</p> } 
-        />
-
-
+  return (
+    <div className="ubication-content">
+      <div className="logo-content">
+        <img className="logo-lp" src={LogoLP} alt="Logo las palmas" />
       </div>
-    );    
-}
 
-export default Ubication
+      <Map
+        googleMapURL={mapUrl}
+        containerElement={
+          <div className="ubication" style={{ height: "300px" }} />
+        }
+        mapElement={<div style={{ height: "100%" }} />}
+        loadingElement={<p>Cargando</p>}
+      />
+    </div>
+  );
+};
+
+export default Ubication;
